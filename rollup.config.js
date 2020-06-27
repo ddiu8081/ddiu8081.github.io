a66ed8942c6a31984ec71f8fa6096ab2bfa4c3d4
ddiu8081 <ddiu8081@gmail.com>
2020-06-27 23:35:51 +0800

    chore: disable css sourcemap in production

--- rollup.config.js
+++ rollup.config.js
@@ -29,7 +29,7 @@ export default {
         }),
       ],
 			css: css => {
-				css.write('public/build/bundle.css');
+				css.write('public/build/bundle.css', !production);
 			}
 		}),
 		resolve({
